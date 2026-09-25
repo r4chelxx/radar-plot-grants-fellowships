9ea38caa2920a4d5d8cc9abc100626e9e44c3d1c
r4chelxx <mercesrachel@gmail.com>
2026-09-25 19:55:55 -0300

    Loader: expose investigation maps

--- data.js
+++ data.js
@@ -1,3 +1,3 @@
 // Compatibility loader. Data is maintained in /data/*.js.
 const P=window.RADAR_PARTS||{};
-window.RADAR={opportunities:P.opportunities||[],stories:P.stories||[],datasets:P.datasets||[],sources:P.sources||[],changelog:P.changelog||[]};
+window.RADAR={opportunities:P.opportunities||[],stories:P.stories||[],datasets:P.datasets||[],sources:P.sources||[],changelog:P.changelog||[],investigations:P.investigations||{}};
