0fcd25df5def0a38da6b72e3fac0705fc6a52477
r4chelxx <mercesrachel@gmail.com>
2026-09-25 21:08:07 -0300

    Add separately classified tools catalog

---
+++ data/tools.js
@@ -0,0 +1,5 @@
+window.RADAR_PARTS=window.RADAR_PARTS||{};
+window.RADAR_PARTS.tools=[
+{id:"datawrapper",name:"Datawrapper",kind:"visualização",url:"https://www.datawrapper.de/",desc:"Criação de gráficos, mapas e tabelas para publicação."},
+{id:"react-simple-maps",name:"React Simple Maps",kind:"biblioteca de mapas",url:"https://www.react-simple-maps.io/",desc:"Componentes SVG para mapas em React; requer arquivos geográficos próprios e implementação no frontend.",discoveredVia:["Quantum of Sollazzo"],discoveryUrl:"https://buttondown.com/puntofisso/archive/676-quantum-of-sollazzo/"}
+];
